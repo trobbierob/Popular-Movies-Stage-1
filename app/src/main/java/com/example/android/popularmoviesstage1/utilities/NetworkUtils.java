23043de1b7e4a7b03d35e27be576c81064950982
trobbierob <troberts@my.hpu.edu>
2018-05-08 00:26:34 -0400

    Add NetworkUtils, menu, and dimensions Strings

---
+++ app/src/main/java/com/example/android/popularmoviesstage1/utilities/NetworkUtils.java
@@ -0,0 +1,62 @@
+package com.example.android.popularmoviesstage1.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public final class NetworkUtils {
+
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String BASE_MOVIE_URL = "";
+
+    private static final String SIZE = "";
+
+    private static final String FILE_PATH = "";
+
+    private static final String API_KEY = "";
+
+    public static URL buildUrl(String locationQuery) {
+        Uri builtUri = Uri.parse(BASE_MOVIE_URL).buildUpon()
+                //.appendQueryParameter()
+                //.appendQueryParameter()
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI " + url);
+
+        return url;
+    }
+
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
