cac7c00c620ebc260f63aaa82d157f04522fd17a
trobbierob <troberts@my.hpu.edu>
2018-05-08 00:34:58 -0400

    Inflate Menu button

--- app/src/main/java/com/example/android/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/android/popularmoviesstage1/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.android.popularmoviesstage1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
-
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
